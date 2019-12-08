8610c74b619d7e79dae0a0505533289951ada137
shi-n <shi-n_xyz@cloudsquare.jp>
2019-12-08 23:27:34 +0900

    Send and Receive

---
+++ ExampleNetwork/SendAndReceive.swift
@@ -0,0 +1,73 @@
+//
+//  SendAndReceive.swift
+//  ExampleNetwork
+//
+//  Created by Shin Inaba on 2019/12/08.
+//  Copyright © 2019 shi-n. All rights reserved.
+//
+
+import Foundation
+import Network
+
+class SendAndReceive {
+    public var running = true
+    private var host:NWEndpoint.Host
+    private var port:NWEndpoint.Port
+    private var nWParameters: NWParameters
+    
+    init(host:NWEndpoint.Host, port:NWEndpoint.Port, nWParameters: NWParameters) {
+        self.host = host
+        self.port = port
+        self.nWParameters = nWParameters
+    }
+
+    func startConnection() {
+        let myQueue = DispatchQueue(label: "ExampleNetwork")
+        let connection = NWConnection(host: host, port: port, using: nWParameters)
+        connection.stateUpdateHandler = { (newState) in
+            switch(newState) {
+            case .ready:
+                print("ready")
+                self.sendMessage(connection)
+            case .waiting(let error):
+                print("waiting")
+                print(error)
+            case .failed(let error):
+                print("failed")
+                print(error)
+            default:
+                print("defaults")
+                break
+            }
+        }
+        connection.start(queue: myQueue)
+        self.receive(nWConnection: connection)
+    }
+
+    func sendMessage(_ connection: NWConnection) {
+        let data = "Example Send Data".data(using: .utf8)
+        let completion = NWConnection.SendCompletion.contentProcessed { (error: NWError?) in
+            print("送信完了")
+//            self.running = false
+        }
+        connection.send(content: data, completion: completion)
+    }
+
+    func receive(nWConnection:NWConnection) {
+        nWConnection.receive(minimumIncompleteLength: 1, maximumLength: 5, completion: { (data, context, flag, error) in
+            print("receiveMessage")
+            if let data = data {
+                let receiveData = [UInt8](data)
+                print(receiveData)
+                print(flag)
+                if(flag == false) {
+                    self.receive(nWConnection: nWConnection)
+                }
+            }
+            else {
+                print("receiveMessage data nil")
+            }
+        })
+    }
+
+}
